31a4d162bb39d6dca27fda9fb69133df4b8a668d
Peti <peter.hathazi99@gmail.com>
2022-09-28 12:20:44 +0200

    Update HogwartsContext.cs

--- Models/HogwartsContext.cs
+++ Models/HogwartsContext.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using HogwartsPotions.Models.Entities;
 using Microsoft.EntityFrameworkCore;
@@ -9,19 +10,31 @@ namespace HogwartsPotions.Models
     public class HogwartsContext : DbContext
     {
         public const int MaxIngredientsForPotions = 5;
+        public DbSet<Student> Students { get; private set; }
+        public DbSet<Room> Rooms { get; private set; }
 
         public HogwartsContext(DbContextOptions<HogwartsContext> options) : base(options)
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().ToTable("students");
+            modelBuilder.Entity<Room>().ToTable("rooms");
+        }
+
         public async Task AddRoom(Room room)
         {
-            throw new NotImplementedException();
+            if (room != null && !this.Rooms.Contains(room))
+            {
+                this.Rooms.Add(room);
+            }
         }
 
         public Task<Room> GetRoom(long roomId)
         {
-            throw new NotImplementedException();
+            var room = Rooms.FirstOrDefaultAsync(room => room.ID.Equals(roomId));
+            return room;
         }
 
         public Task<List<Room>> GetAllRooms()
