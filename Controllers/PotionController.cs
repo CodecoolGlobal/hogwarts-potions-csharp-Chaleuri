f7c3647581f868f55435007293a71e16a6f87022
Peti <peter.hathazi99@gmail.com>
2022-09-30 14:20:33 +0200

    Update PotionController.cs

--- Controllers/PotionController.cs
+++ Controllers/PotionController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HogwartsPotions.Controllers
 {
+    [Route("/potions")]
     public class PotionController : Controller
     {
         private readonly HogwartsContext _context;
@@ -16,10 +17,16 @@ namespace HogwartsPotions.Controllers
             _context = context;
         }
 
-        [HttpGet("/potions")]
+        [HttpGet]
         public async Task<List<Potion>> GetAllPotions()
         {
             return await _context.GetAllPotions();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> BrewPotion()
+        {
+            return await _context.BrewPotion();
         }
     }
 }
