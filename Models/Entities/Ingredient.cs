0685f05351570185a34359820b6e5a5ff38c3ae4
Peti <peter.hathazi99@gmail.com>
2022-09-30 12:46:14 +0200

    Create Ingredient.cs

---
+++ Models/Entities/Ingredient.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace HogwartsPotions.Models.Entities;
+
+public class Ingredient
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public long ID { get; set; }
+    public string Name { get; set; }
+
+    [JsonIgnore]
+    public HashSet<Recipe> Recipes { get; set; } = new();
+    [JsonIgnore]
+    public HashSet<Potion> Potions { get; set; } = new();
+}
