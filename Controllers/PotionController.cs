abcdc73e0611bcb608ff7dabe1ae486cf9e38245
Peti <peter.hathazi99@gmail.com>
2022-09-30 14:12:55 +0200

    Create PotionController.cs

---
+++ Controllers/PotionController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HogwartsPotions.Models;
+using HogwartsPotions.Models.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HogwartsPotions.Controllers
+{
+    public class PotionController : Controller
+    {
+        private readonly HogwartsContext _context;
+
+        public PotionController(HogwartsContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("/potions")]
+        public async Task<List<Potion>> GetAllPotions()
+        {
+            return await _context.GetAllPotions();
+        }
+    }
+}
